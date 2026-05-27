c26e4727f6aca3a184828a4c361c910a1ab9598a
klakson1306 <leonardoirdy@gmail.com>
2026-05-27 18:38:52 +0200

    macinando

--- result_an.py
+++ result_an.py
@@ -11,13 +11,14 @@ def exp_fun(x,a,b):
     return a*np.exp(b*(x)) # from WKB approximation, x is proportional to the energy barrier height
 
 param_opt, param_cov = curve_fit(exp_fun, x_data, y_data, p0=[y_data[0], 10])
-
+print(f'fitted parameters: a = {param_opt[0]}, b = {param_opt[1]}')
 x_fit = np.linspace(min(x_data), max(x_data), 100)
 y_fit = exp_fun(x_fit, param_opt[0], param_opt[1])
 
 plt.figure(figsize=(8, 6))
-plt.plot(x_fit, y_fit,'-', label='fit: a*exp(bx)')
-plt.plot(x_data, y_data, 'o', label='numerical data')
+plt.grid()
+plt.plot(x_fit, y_fit,'-', label='Exponential fit')
+plt.plot(x_data, y_data, 'o', label='Experimental data')
 plt.xlabel('Barrier gate voltage [V]')
 plt.ylabel('Energy splitting [eV]')
 plt.legend()
